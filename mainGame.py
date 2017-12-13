6f376789e2bac4ac9c9d9ff7bc053a1a0b23d615
Gustavo Scharf <scharfgustavo@gmail.com>
2017-12-13 01:44:21 -0500

    added towers,enemies,health
    OK big update
    
    - Added towers, you now need gold to place the towers and you get gold from killing enemies
    - added primitive levels and waves system (only level 1 currently, doesn't switch)
    - enemies now spawn in increasing numbers
    - added gold / score / health
    - there are nice buttons at the bottom of screen
    - game ends when you lose all life
    
    what's next?
    
    well,
    
    - add pathing so enemies can move around,
    - fix bug where bullet doesn't get deleted (i think it's because two towers are targetting the same enemy at once.. i gotta look into that)
    - make some more levels
    - make save game/load game system
    - create more levels and maybe some storyline
    - lots more! these are just what I feel like are the logical next steps

--- mainGame.py
+++ mainGame.py
@@ -5,6 +5,11 @@ from random import randint, uniform
 import time
 import os
 
+### these arrays are global variables that house
+### the objects for enemies and for towers
+towers = []
+enemies = []
+
 class Cell():
 	def __init__(self,x,y):
 		self.x = x
@@ -21,7 +26,6 @@ class Cell():
 
 	def drawGrid(self):
 		# windows.gameCanvas.create_rectangle(self.drawX,self.drawY,self.drawX+windows.w,self.drawY+windows.w)
-
 		if self.wall == True:
 			windows.gameCanvas.create_rectangle(self.drawX,self.drawY,self.drawX+windows.w,self.drawY+windows.w,fill="black")
 		# if self.X == True:
@@ -50,7 +54,6 @@ def keyPress(event):
 
 def placeTower(x,y):
 	global towers
-	towers = []
 	towers.append(Tower(x,y))
 	towers[len(towers)-1].draw()
 	player.gold -= 250
@@ -78,24 +81,211 @@ def leftClick(event):
 		if isSuitableLocationForTower(event.x,event.y) == True:
 			placeTower(event.x,event.y)
 
+def createEnemies(amount):
+	global enemies
+	### first, pick a random starting cell from list of cells
+	startingCells = getStartingCells()
+	### then spawn it somewhere in that cell
+	for enemy in range(amount):
+		whichOne = randint(0,len(startingCells)-1)
+		x = (startingCells[whichOne].x * windows.w)+randint(10,40)
+		y = (startingCells[whichOne].y * windows.w)+randint(10,40)
+		enemies.append(Enemy(x,y))
+
+def getStartingCells():
+	listOfStartCells = []
+	for cell in cells:
+		if cell.start == True:
+			listOfStartCells.append(cell)
+	return(listOfStartCells)
+
+def drawEnemies():
+	for enemy in enemies:
+		if enemy.active == False:
+			enemy.draw()
+
+def moveEnemies():
+	for enemy in enemies:
+		enemy.moveForward()
+		enemy.active = True
+
+def getEndingCells():
+	listofEndingCells = []
+	for cell in cells:
+		if cell.end == True:
+			listofEndingCells.append(cell)
+	return(listofEndingCells)
+
+def checkForRunawayEnemies():
+	doIt = False
+	for enemy in enemies:
+		if enemy.x > 400:
+			doIt = True
+			break
+	### enemies that make it to the end of the screen
+	if doIt == True:
+		endingCells = getEndingCells()
+		listOfCoordsToCheck = []
+		for cell in endingCells:
+			x = (cell.x*windows.w)+(windows.w/2)+15
+			y = (cell.y*windows.w)+(windows.w/2)
+			listOfCoordsToCheck.append((x,y))
+
+		for enemy in enemies:
+			for coords in listOfCoordsToCheck:
+				if dist(enemy.x,enemy.y,coords[0],coords[1])<25:
+					removeEnemy(enemy)
+					player.deleteHealth()
+					player.health -= 1
+					player.drawHealth()
+					break
+
+def quitGame():
+	messageBox.quit()
+	messageBox.destroy()
+	windows.gameWindow.quit()
+	windows.gameWindow.destroy()
+
+
+def checkForDefeat():
+	global messageBox
+	if player.health < 1:
+		messageBox = Tk()
+		messageBox.title('Game Over!')
+		##### fix this later
+		messageBox.geometry('300x65+300+300')
+		labelText = "Your Score: "+str(player.score)
+		label = Label(messageBox,text=labelText,font='Helvetica 16 bold',bg='black',fg='white')
+		label.pack(fill='both')
+		button = Button(messageBox,text='Ok',command=quitGame,bg='black',fg='white',height=3)
+		button.pack(fill='both')
+		messageBox.mainloop()
+
+
+
+def removeEnemy(enemy):
+	enemy.delete()
+	enemies.pop(enemies.index(enemy))
+
+def towerLogic():
+	for tower in towers:
+		tower.getEnemiesInRange()
+		tower.updateTick()
+		tower.shouldIDelete()
+		tower.shouldIShoot()
+		tower.checkLockedOnRange()
+
+class Enemy():
+	def __init__(self,x,y):
+		self.x = x
+		self.y = y
+		self.direction = 'right'
+		self.speed = uniform(.05,.1)
+		self.active = False
+
+	def draw(self):
+		self.drawnImage = windows.gameCanvas.create_text(self.x,self.y,text="X")
+
+	def delete(self):
+		windows.gameCanvas.delete(self.drawnImage)
+
+	def moveForward(self):
+		self.delete()
+		if self.direction == 'right':
+			self.x += self.speed
+		if self.direction == 'up':
+			self.y -= self.speed
+		if self.direction == 'down':
+			self.y += self.speed
+		self.draw()
+
 class Player():
 	def __init__(self):
 		self.gold = 500
 		self.score = 0
 		self.currentSelection = "none"
+		self.health = 5
+		self.healthImages = []
+
+	def drawHealth(self):
+		x = 410
+		y = 10
+		for z in range(self.health):
+			newX = x+((z+1)*15)
+			self.healthImages.append(windows.gameCanvas.create_rectangle(newX,y,(newX+10),(y+10),fill='red'))
+
+	def deleteHealth(self):
+		for image in self.healthImages:
+			windows.gameCanvas.delete(self.healthImages)
+		##### need to redraw top right hand corner cell(8,0) and cell(9,0)
+		cells[findCellByXY(8,0)].drawGrid()
+		cells[findCellByXY(9,0)].drawGrid()
+		self.healthImages = []
 
 class Tower():
 	def __init__(self,x,y):
 		self.x = x-7
 		self.y = y-7
+		self.lockedOn = False
+		self.reloadRate = 600
+		self.currentRate = 0
+		self.bulletRate = 0
+		self.bulletImages = []
 
 	def draw(self):
 		self.drawnImage = windows.gameCanvas.create_oval(self.x,self.y,self.x+15,self.y+15,fill='blue')
 
+	def getEnemiesInRange(self):
+		for enemy in enemies:
+			if dist(enemy.x,enemy.y,self.x,self.y)<75:
+				self.lockedOn = enemy
+				break
+
+	def checkLockedOnRange(self):
+		if self.lockedOn != False:
+			if dist(self.x,self.y,self.lockedOn.x,self.lockedOn.y)>75:
+					self.lockedOn = False
+					print('enemy got away')
+
+	def updateTick(self):
+		self.currentRate += 1
+		self.bulletRate += .3
+
+	def shouldIDelete(self):
+		if self.bulletRate > self.reloadRate:
+			if len(self.bulletImages)>0:
+				self.deleteBullet()
+			self.bulletRate = 0
+
+	def shouldIShoot(self):
+		if self.lockedOn != False:
+			print(self.bulletRate)
+			if self.currentRate > self.reloadRate:
+				self.shoot()
+				self.currentRate = 0
+
+	def shoot(self):
+		self.drawBullet(self.lockedOn.x,self.lockedOn.y)
+		if self.lockedOn in enemies:
+			removeEnemy(self.lockedOn)
+			self.lockedOn = False
+			player.score += 10
+			player.gold += 25
+			windows.updateLabels()
+
+	def drawBullet(self,x,y):
+		self.bulletImages.append(windows.gameCanvas.create_line(self.x,self.y,x,y,width=2))
+		print(self.bulletImages)
+
+	def deleteBullet(self):
+		for bullet in self.bulletImages:
+			windows.gameCanvas.delete(bullet)
+		self.bulletImages = []
 
 class WindowHandler():
 	def __init__(self):
 		self.currentLevel = None
+		self.numOfEnemies = 5
 		
 	def startMainMenu(self):
 		### starts up the main menu, makes the buttons
@@ -136,6 +326,7 @@ class WindowHandler():
 		self.goldLabel['text'] = labelText
 		if player.gold == 0:
 			self.goldLabel['fg'] = 'red'
+
 	def startGameWindow(self):
 		### initalize the game window
 		self.exitMainMenu()
@@ -166,7 +357,10 @@ class WindowHandler():
 		self.levelPicker('level 1')
 
 	def startWave(self):
-		pass
+		self.currentSelection = 'none'
+		createEnemies(self.numOfEnemies)
+		drawEnemies()
+		self.numOfEnemies += 1
 
 	def clickedBuyTower(self):
 		player.currentSelection = 'buy tower'
@@ -178,13 +372,25 @@ class WindowHandler():
 		currentGameTick = 0
 		while True:
 			currentGameTick += 1
+			moveEnemies()
+			checkForRunawayEnemies()
+			towerLogic()
+			checkForDefeat()
 			self.gameWindow.update()
 			self.gameWindow.update_idletasks()
+
+	def updateLabels(self):
+		labelText = "Score: "+str(player.score)
+		self.scoreLabel['text'] = labelText
+		labelText = "Gold: "+str(player.gold)+'gp'
+		self.goldLabel['text'] = labelText
+		self.goldLabel['fg'] = 'black'
 
 	def loadLevel(self):
 		### sets up the cells and draws everything once
 		for cell in cells:
 			cell.drawGrid()
+		player.drawHealth()
 		self.mainGameLoop()
 
 
